ff356eb7e5f544bfadb30576a0e530d7b73214e3
spendist <spender@163.com>
2017-02-05 08:05:11 +0800

    Changes to be committed: 	new file:   c/9x9.c  Changes not staged for commit: 	deleted:    c/a.out

---
+++ c/9x9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+	int i,j;
+	for (i=1;i<10;i++) {
+		for (j=1;j<i+1;j++) {
+			printf("%dx%d=%d  ",i,j,i*j);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return (0);
+}
